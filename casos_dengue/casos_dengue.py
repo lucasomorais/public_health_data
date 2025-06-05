4d05cf304db877c1bbbc696ad55baa24c9d3f349
lucashyko <lucasolivermorais@gmail.com>
2025-06-05 11:11:23 -0300

    --

--- casos_dengue/casos_dengue.py
+++ casos_dengue/casos_dengue.py
@@ -6,6 +6,8 @@ from modules.data_utils import load_string_data, save_string_data, compare_data,
 from playwright.sync_api import sync_playwright
 from datetime import datetime
 
+DATA_FILE = "casos_dengue/casos_dengue.yaml"  # ajuste o caminho conforme desejar
+
 def access_website(p):
     browser = p.chromium.launch(headless=False)
     context = browser.new_context(accept_downloads=True)
@@ -17,21 +19,20 @@ def access_website(p):
 
     return page, browser
 
-
-
 def fetch_data(page):
     rodape = page.query_selector("div.rodape_htm")
     if rodape:
         rodape.scroll_into_view_if_needed()
         
-        # Seleciona todos os <li> dentro de <ol> dentro de rodape_htm
         itens = rodape.query_selector_all("ol > li")
         
         lista_textos = []
         for item in itens:
             texto = item.inner_text().strip()
             if texto.startswith("Dados de 20"):
-                lista_textos.append(texto)
+                # Mantém só até a primeira quebra de linha ou asterisco
+                texto_limpo = re.split(r'\n|\*', texto)[0].strip()
+                lista_textos.append(texto_limpo)
         
         print(f"Items encontrados: {lista_textos}")
         return lista_textos
@@ -40,14 +41,30 @@ def fetch_data(page):
         return []
 
 
-    
 def main():
-
     print(f"Running check at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
     with sync_playwright() as p:
         page, browser = access_website(p)
         current_data = fetch_data(page)
+
+        if not current_data:
+            print("No data fetched. Exiting.")
+            browser.close()
+            return
+
+        stored_data = load_string_data(DATA_FILE)  or []
+
+        differences = compare_data(current_data, stored_data)
+
+        if differences:
+            print("New data detected! Proceeding with download.")
+            fetch_data(page)
+            save_string_data(current_data, DATA_FILE)
+        else:
+            print("No new data detected.")
+
+        browser.close()
 
 if __name__ == "__main__":
     main()
